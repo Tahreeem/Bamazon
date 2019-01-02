085544294856b021b0ab7b4e95beae2671e1aff4
Tahreem <tahreemsohailbutt@gmail.com>
2019-01-02 04:49:05 -0500

    improved user experience as well as logic; increased separation of functions and included recursion for questions that need to be repeated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,83 +1,91 @@
-var mysql = require("mysql");
+﻿var mysql = require("mysql");
 var inquirer = require("inquirer");
+require('dotenv').config();
+
+var questions = [{
+    name: "productID",
+    type: "input",
+    message: "What is the ID of the product you would like to buy?",
+    validate: ValidateNumber
+},
+{
+    name: "quantity",
+    type: "input",
+    message: "What is the quantity of the product you would like to buy?",
+    validate: ValidateNumber
+}]
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your port; if not 3306
-    port: 3306,
-
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "something",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (error) {
     if (error) throw error;
-    else console.log("nothing?");
-    // run the start function after the connection is made to prompt the user
 
-    inquirer
-        .prompt([{
-            name: "itemID",
-            type: "input",
-            message: "What is the ID of the product you would like to buy?",
-            validate: ValidateNumber
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "What is the quantity of the product you would like to buy?",
-            validate: ValidateNumber
-        }])
-        .then(function (answer) {
+    printAllItems()
+        .then(function () {
 
-            connection.query(
-                "SELECT * FROM bamazon_db.products WHERE ?",
-                [{
-                    item_id: answer.itemID
-                }],
-                function (error, results) {
-                    if (error) throw error;
-
-                    if (results.length == 0) {
-                        console.log("Product not found, please enter an ID that exists");
-                    }
-                    else if (answer.quantity > results[0].stock_quantity) {
-                        console.log(`Insufficient quantity of product available. Only ${answer.quantity} units available. Please enter a lower number`);
-                    }
-                    else {
-                        UpdateStock({ stock_quantity: results[0].stock_quantity - answer.quantity }, { item_id: answer.itemID });
-                        printItem(results);
-                    }
-                }
-            );
+            QuestionZeroHandling()
+                .then(function (resultZero) {
+                    QuestionOneHandling(resultZero);
+                });
 
         });
-
-    
-
-
 });
 
 
+function printAllItems() {
 
-function printItem(item) {
-    console.log("Item ID : " + item[0].item_id);
-    console.log("Product Name : " + item[0].product_name);
-    console.log("Department Name : " + item[0].department_name);
-    console.log("Price : " + item[0].price);
-    console.log("Stock Quantity : " + item[0].stock_quantity); 
+    console.log("Here are all the products:");
+
+    return new Promise(resolve => {
+
+        connection.query(
+            "SELECT * FROM bamazon_db.products",
+            function (error, results) {
+                if (error) throw error;
+
+                if (results.length == 0) {
+                    console.log("No products to show ¯\_(ツ)_/¯");
+                }
+                else {
+                    printItem(results);
+                }
+
+
+                resolve();
+            });
+
+    });
+}
+
+function printItem(items) {
+
+    return new Promise(resolve => {
+
+        items.forEach(function (currentItem) {
+            console.log("\nItem ID : " + currentItem.item_id);
+            console.log("Product Name : " + currentItem.product_name);
+            console.log("Department Name : " + currentItem.department_name);
+            console.log("Price : " + currentItem.price);
+            console.log("Stock Quantity : " + currentItem.stock_quantity);
+            console.log("____________________________________");
+        });
+
+
+        resolve();
+    });
 }
 
 
-
-function ValidateNumber (value) {
+function ValidateNumber(value) {
     if (!isNaN(value) && value % 1 == 0 && parseInt(value) > 0) return true;
     else {
         console.log("\nPlease enter a valid number\n");
@@ -85,7 +93,35 @@ function ValidateNumber (value) {
     }
 }
 
+function askQuestions(whichQuestions) {
 
+    return new Promise(resolve => {
+        inquirer
+            .prompt(whichQuestions)
+            .then(function (answers) {
+                resolve(answers);
+            });
+    });
+
+}
+
+
+function GetProduct(productID) {   //answers to inquirer questions
+
+    return new Promise(resolve => {
+
+        connection.query(
+            "SELECT * FROM bamazon_db.products WHERE ?",
+            [{
+                item_id: productID
+            }],
+            function (error, resultZero) {
+                if (error) throw error;
+
+                resolve(resultZero);
+            });
+    });
+}
 
 function UpdateStock(set, where) {
     var parameters = [];
@@ -96,9 +132,56 @@ function UpdateStock(set, where) {
         parameters,
         function (error, results) {
             if (error) throw error;
-            if (results.affectedRows>0) {
+            if (results.affectedRows > 0) {
                 connection.end();
             }
         }
     );
 }
+
+
+function QuestionOneHandling(resultZero) {
+
+    return new Promise(resolve => {
+
+        askQuestions(questions[1])
+            .then(function (answerOne) {
+
+                if (answerOne.quantity > resultZero[0].stock_quantity) {
+                    console.log(`Insufficient quantity of product available. Only ${resultZero[0].stock_quantity} units available. Please enter a lower number...`);
+                    QuestionOneHandling(resultZero);
+                }
+                else {
+                    resultZero[0].stock_quantity = resultZero[0].stock_quantity - answerOne.quantity;  //updating locally
+                    UpdateStock({ stock_quantity: resultZero[0].stock_quantity }, { item_id: resultZero[0].item_id }); //updating globally or in database
+                    console.log("\nHere is the updated stock:");
+                    printItem(resultZero);
+                }
+
+                resolve();
+            });
+    });
+}
+
+function QuestionZeroHandling() {
+
+    return new Promise(resolve => {
+
+        console.log("\n\n");
+
+        askQuestions(questions[0])
+            .then(function (answerZero) {
+
+                GetProduct(answerZero.productID)
+                .then(resultZero => {
+
+                if (resultZero.length == 0) {
+                    console.log("Product not found. Please enter an ID that exists...");
+                    QuestionZeroHandling();
+                }
+                else resolve(resultZero); 
+                });
+            });
+    });
+}
+
